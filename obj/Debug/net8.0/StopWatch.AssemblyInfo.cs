aadbbcea244dc3992fd2573dcfed9280aa7a3563
Ryan Lirio <ryanlirio2@gmail.com>
2024-09-19 15:15:02 -0300

    first commit

--- obj/Debug/net8.0/StopWatch.AssemblyInfo.cs
+++ obj/Debug/net8.0/StopWatch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15dc4de384eb57c8e293abd3e5b316683ca1e506")]
 [assembly: System.Reflection.AssemblyProductAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
